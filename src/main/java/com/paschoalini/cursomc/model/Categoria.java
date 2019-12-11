701c98e2f1853d738e6357a6d1f98cbd5e4827b2
= <=>
2019-12-11 17:39:20 -0300

    Banco de dados MySQL e mapeamento da tabela Categoria

--- src/main/java/com/paschoalini/cursomc/model/Categoria.java
+++ src/main/java/com/paschoalini/cursomc/model/Categoria.java
@@ -2,9 +2,23 @@ package com.paschoalini.cursomc.model;
 
 import java.io.Serializable;
 
-public class Categoria implements Serializable {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotEmpty(message = "CAMPO OBRIGATÓRIO: Nome da categoria")
+	@Size(min=3, max=45)
 	private String nome;
 	
 	public Categoria() {
