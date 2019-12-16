6549dd4d3b59edffdaaf670e951679ac35b7e207
= <=>
2019-12-16 19:29:45 -0300

    Personalizando o retorno da validação

--- src/main/java/com/paschoalini/cursomc/domain/Categoria.java
+++ src/main/java/com/paschoalini/cursomc/domain/Categoria.java
@@ -9,8 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 
 @Entity
 public class Categoria implements Serializable{
